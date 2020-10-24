47a5aa299a8eca64132352359b0eafe77f2e1aad
ben4932042 <63529371+ben4932042@users.noreply.github.com>
2020-10-24 22:46:51 +0800

    Update useragent creater

--- Testing_fake_useragent/spiders/useragent_creater.py
+++ Testing_fake_useragent/spiders/useragent_creater.py
@@ -1,10 +1,10 @@
 """
 Generate the fake user agent to scrapy.
 """
-#from fake_useragent import UserAgent
-#ua = UserAgent()
+from fake_useragent import UserAgent
+ua = UserAgent()
 # ie useragent
-#:print(ua.ie)
+print(ua.ie)
 # chrome useragent
 #print(ua.chrome)
 #
